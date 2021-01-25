749752f82f0b61b64a5fd9637529845e41f17ce8
Alice Lepissier <alice.lepissier@gmail.com>
2021-01-24 19:36:19 -0800

    Rework with latest classifications + HDI + new centroids

---
+++ Check country codes.R
@@ -0,0 +1,144 @@
+# Check country codes
+# Alice Lepissier
+# alice.lepissier@gmail.com
+
+## ## ## ## ## ## ## ## ## ## ##
+# INDEX                     ####
+## ## ## ## ## ## ## ## ## ## ##
+# Preamble
+# Check UN regions
+# Check World Bank regions
+# Check IMF codes
+# Import centroids
+
+
+
+## ## ## ## ## ## ## ## ## ## ##
+# PREAMBLE                  ####
+## ## ## ## ## ## ## ## ## ## ##
+
+library(here)
+setwd(here())
+library(CoordinateCleaner)
+library(openxlsx)
+library(readxl)
+library(tidyverse)
+
+
+
+## ## ## ## ## ## ## ## ## ## ##
+# CHECK UN REGIONS          ####
+## ## ## ## ## ## ## ## ## ## ##
+
+codes <- read_xlsx("Codes_Masterlist.xlsx", sheet = "Codes") %>%
+  select(ISO3166.3, Country, 
+         UN_Region, UN_Region_Code, 
+         `UN_Sub-region`, `UN_Sub-region_Code`, 
+         UN_Intermediate_Region, UN_Intermediate_Region_Code, 
+         UN_M49_Code)
+UNSD <- read_xlsx("2020_UNSD.xlsx", sheet = "Sheet1")
+
+codes <- left_join(codes, UNSD %>% 
+                     select(`ISO-alpha3 Code`, New_UN_Region = `Region Name`, New_UN_Region_Code = `Region Code`),
+                   by = c("ISO3166.3" = "ISO-alpha3 Code"))
+
+codes %>% filter(UN_Region != New_UN_Region)
+codes %>% filter(UN_Region_Code != New_UN_Region_Code)
+
+codes <- left_join(codes, UNSD %>% 
+                     select(`ISO-alpha3 Code`, 
+                            `New_UN_Sub-region` = `Sub-region Name`, 
+                            `New_UN_Sub-region_Code` = `Sub-region Code`),
+                   by = c("ISO3166.3" = "ISO-alpha3 Code"))
+
+codes %>% filter(`UN_Sub-region` != `New_UN_Sub-region`)
+codes %>% filter(`UN_Sub-region_Code` != `New_UN_Sub-region_Code`)
+
+codes <- left_join(codes, UNSD %>% 
+                     select(`ISO-alpha3 Code`, 
+                            New_UN_Intermediate_Region = `Intermediate Region Name`, 
+                            New_UN_Intermediate_Region_Code = `Intermediate Region Code`),
+                   by = c("ISO3166.3" = "ISO-alpha3 Code"))
+
+codes %>% filter(UN_Intermediate_Region != New_UN_Intermediate_Region)
+codes %>% filter(UN_Intermediate_Region_Code != New_UN_Intermediate_Region_Code)
+
+codes <- left_join(codes, UNSD %>% 
+                     select(`ISO-alpha3 Code`, New_UN_M49_Code = `M49 Code`),
+                   by = c("ISO3166.3" = "ISO-alpha3 Code"))
+
+codes$New_UN_M49_Code <- str_remove(codes$New_UN_M49_Code, "^0+")
+codes %>% filter(UN_M49_Code != New_UN_M49_Code)
+
+
+
+## ## ## ## ## ## ## ## ## ## ##
+# CHECK WORLD BANK REGIONS  ####
+## ## ## ## ## ## ## ## ## ## ##
+
+codes <- read_xlsx("Codes_Masterlist.xlsx", sheet = "Codes") %>% 
+  select(ISO3166.3, Country, WB_Region)
+
+WB <- read_xls("2020_World Bank.xls", skip = 4) %>% 
+  select(Country = Economy, 
+         ISO3166.3 = Code, 
+         New_WB_Region = Region) %>%
+  filter(Country != "x")
+
+codes <- left_join(codes, WB %>% 
+                     select(ISO3166.3, New_WB_Region),
+                   by = c("ISO3166.3"))
+
+codes %>% filter(WB_Region != New_WB_Region)
+
+
+
+## ## ## ## ## ## ## ## ## ## ##
+# CHECK IMF CODES           ####
+## ## ## ## ## ## ## ## ## ## ##
+
+codes <- read_xlsx("Codes_Masterlist.xlsx", sheet = "Codes") %>% 
+  select(ISO3166.3, Country, IMF_Code)
+IMF <- read_xlsx("IMF codes.xlsx", sheet = "Sheet1", skip = 1) %>%
+  select(1:3)
+
+codes <- left_join(codes, IMF %>% 
+                     select(`ISO Code`, New_IMF_Code = `IMF Code`),
+                   by = c("ISO3166.3" = "ISO Code"))
+
+codes %>% filter(IMF_Code != New_IMF_Code)
+
+
+
+## ## ## ## ## ## ## ## ## ## ##
+# IMPORT CENTROIDS          ####
+## ## ## ## ## ## ## ## ## ## ##
+
+codes <- read_xlsx("Codes_Masterlist.xlsx", sheet = "Codes")
+regional <- read_xlsx("Codes_Masterlist.xlsx", 
+                      sheet = "UN_Regional_Groupings")
+wb <- read_xlsx("Codes_Masterlist.xlsx", 
+                sheet = "World_Bank_Groupings")
+
+data(countryref)
+
+codes <- left_join(codes, countryref %>%
+                     select(iso3, Centroid_Longitude = centroid.lon, Centroid_Latitude = centroid.lat),
+                   by = c("ISO3166.3" = "iso3"))
+
+book <- createWorkbook()
+hs <- createStyle(textDecoration = "Bold")
+
+addWorksheet(book, "Codes")
+writeData(book, sheet = "Codes", 
+          x = codes, headerStyle = hs)
+
+addWorksheet(book, "UN_Regional_Groupings")
+writeData(book, sheet = "UN_Regional_Groupings",
+          x = regional, headerStyle = hs)
+
+addWorksheet(book, "World_Bank_Groupings")
+writeData(book, sheet = "World_Bank_Groupings", 
+          x = wb, headerStyle = hs)
+
+saveWorkbook(book, "Codes_Masterlist.xlsx", overwrite = TRUE)
---
+++ Import latest country classifications.R
@@ -0,0 +1,196 @@
+# Import latest country classifications
+# Alice Lepissier
+# alice.lepissier@gmail.com
+
+## ## ## ## ## ## ## ## ## ## ##
+# INDEX                     ####
+## ## ## ## ## ## ## ## ## ## ##
+# Preamble
+# Import existing sheets
+# Merge latest UNSD groups
+# Merge latest HDI groups
+# Merge latest World Bank groups
+# Create workbook
+
+
+
+## ## ## ## ## ## ## ## ## ## ##
+# PREAMBLE                  ####
+## ## ## ## ## ## ## ## ## ## ##
+
+library(here)
+setwd(here())
+library(openxlsx)
+library(readxl)
+library(tidyverse)
+
+
+
+## ## ## ## ## ## ## ## ## ## ##
+# IMPORT EXISTING SHEETS    ####
+## ## ## ## ## ## ## ## ## ## ##
+
+codes <- read_xlsx("Codes_Masterlist.xlsx", sheet = "Codes") %>%
+  select(-c(WB_Other, WB_Lending_Category))
+regional <- read_xlsx("Codes_Masterlist.xlsx", 
+                      sheet = "UN_Regional_Groupings")
+wb <- read_xlsx("Codes_Masterlist.xlsx", 
+                sheet = "World_Bank_Groupings")
+
+
+
+## ## ## ## ## ## ## ## ## ## ##
+# MERGE LATEST UNSD GROUPS  ####
+## ## ## ## ## ## ## ## ## ## ##
+
+# 2020 classification
+UNSD <- read_xlsx("2020_UNSD.xlsx", sheet = "Sheet1") %>%
+  select(Country = `Country or Area`,
+         ISO3166.3 = `ISO-alpha3 Code`,
+         New_UN_LDC = `Least Developed Countries (LDC)`,
+         New_UN_LLDC = `Land Locked Developing Countries (LLDC)`,
+         New_UN_SIDS = `Small Island Developing States (SIDS)`,
+         `New_UN_Developing-Developed` = `Developed / Developing Countries`) %>%
+  mutate_at(c("New_UN_LDC", "New_UN_LLDC", "New_UN_SIDS"),
+            ~ifelse(. == "x", 1, .)) %>%
+  mutate_at(c("New_UN_LDC", "New_UN_LLDC", "New_UN_SIDS"),
+            ~ifelse(is.na(.), 0, .))
+
+codes <- left_join(codes, UNSD %>% 
+                     select(-Country),
+                   by = c("ISO3166.3"))
+
+codes %>% 
+  select(Country,
+         UN_LDC, New_UN_LDC) %>%
+  filter(UN_LDC != New_UN_LDC) %>% 
+  arrange(Country)
+
+codes %>% 
+  select(Country,
+         UN_LLDC, New_UN_LLDC) %>%
+  filter(UN_LLDC != New_UN_LLDC) %>% 
+  arrange(Country)
+
+codes %>% 
+  select(Country,
+         UN_SIDS, New_UN_SIDS) %>%
+  filter(UN_SIDS != New_UN_SIDS) %>% 
+  arrange(Country)
+
+codes %>% 
+  select(Country,
+         `UN_Developing-Developed`, `New_UN_Developing-Developed`) %>%
+  filter(`UN_Developing-Developed` != `New_UN_Developing-Developed`) %>% 
+  arrange(Country)
+
+codes <- codes %>%
+  select(-c(UN_LDC, UN_LLDC, UN_SIDS, `UN_Developing-Developed`))
+colnames(codes) <- gsub("New_", "", colnames(codes))
+
+
+
+## ## ## ## ## ## ## ## ## ## ##
+# MERGE LATEST HDI GROUPS   ####
+## ## ## ## ## ## ## ## ## ## ##
+
+UNDP <- read_xlsx("2020_UNDP.xlsx", sheet = "Table 1", skip = 6) %>%
+  select(Country = ...2,
+         HDI = `2019...3`) %>%
+  mutate(HDI = as.numeric(HDI)) %>%
+  filter(!is.na(HDI))
+
+UNDP <- UNDP %>%
+  mutate(New_UNDP_HDI_Group = ifelse(HDI < 0.550, "Lo HDI",
+                                     ifelse(HDI >= 0.550 & HDI <= 0.699, "Med HDI",
+                                            ifelse(HDI >= 0.700 & HDI <= 0.799, "Hi HDI",
+                                                   ifelse(HDI >= 0.800, "VHi HDI", NA)))))
+
+nogroup <- UNDP %>% filter(is.na(New_UNDP_HDI_Group))
+
+UNDP <- left_join(UNDP, codes %>%
+                    select(Country, ISO3166.3),
+                  by = c("Country"))
+UNDP %>% filter(is.na(ISO3166.3))
+
+codes <- left_join(codes, UNDP %>%
+                     select(ISO3166.3, New_UNDP_HDI_Group) %>%
+                     filter(!is.na(ISO3166.3)),
+                   by = c("ISO3166.3"))
+
+changed <- codes %>% 
+  select(Country, UNDP_HDI_Group, New_UNDP_HDI_Group) %>%
+  filter(UNDP_HDI_Group != New_UNDP_HDI_Group) %>% 
+  arrange(Country)
+
+codes <- codes %>%
+  select(-UNDP_HDI_Group) %>%
+  rename(UNDP_HDI_Group = New_UNDP_HDI_Group)
+
+
+
+## ## ## ## ## ## ## ## ## ## ## ## ##
+# MERGE LATEST WORLD BANK GROUPS  ####
+## ## ## ## ## ## ## ## ## ## ## ## ##
+
+# June 2020 classification
+WB <- read_xls("2020_World Bank.xls", skip = 4) %>% 
+  select(Country = Economy, 
+         ISO3166.3 = Code, 
+         New_WB_Income_Group = `Income group`,
+         WB_Lending_Category = `Lending category`, 
+         WB_Other = Other) %>%
+  filter(Country != "x")
+
+WB <- WB %>%
+  mutate(New_WB_Income_Group = ifelse(New_WB_Income_Group == "Low income", "LIC",
+                                      ifelse(New_WB_Income_Group == "Lower middle income", "LMC",
+                                             ifelse(New_WB_Income_Group == "Upper middle income", "UMC",
+                                                    ifelse(New_WB_Income_Group == "High income", "HIC", NA)))))
+
+nogroup <- WB %>% filter(is.na(New_WB_Income_Group))
+
+codes <- left_join(codes, WB %>% 
+                     select(ISO3166.3, New_WB_Income_Group, WB_Lending_Category, WB_Other),
+                   by = c("ISO3166.3"))
+
+changed <- codes %>% 
+  select(Country, WB_Income_Group, New_WB_Income_Group) %>%
+  filter(WB_Income_Group != New_WB_Income_Group) %>% 
+  arrange(Country)
+
+codes <- codes %>%
+  select(-WB_Income_Group) %>%
+  rename(WB_Income_Group = New_WB_Income_Group)
+
+
+
+## ## ## ## ## ## ## ## ## ## ##
+# CREATE WORKBOOK           ####
+## ## ## ## ## ## ## ## ## ## ##
+
+codes <- codes %>%
+  select(ISO3166.3, ISO3166.2, Country,
+         UN_Region, UN_Region_Code, `UN_Sub-region`, `UN_Sub-region_Code`, UN_Intermediate_Region, UN_Intermediate_Region_Code,
+         UN_M49_Code, UN_LDC, UN_LLDC, UN_SIDS, `UN_Developing-Developed`, UNDP_HDI_Group,
+         WB_Income_Group, WB_Region, WB_Lending_Category, WB_Other,
+         IMF_Code, OECD, EU27, `Arab League`, Centroid_Longitude, Centroid_Latitude)
+codes <- codes %>%
+  arrange(ISO3166.3)
+
+book <- createWorkbook()
+hs <- createStyle(textDecoration = "Bold")
+
+addWorksheet(book, "Codes")
+writeData(book, sheet = "Codes", 
+          x = codes, headerStyle = hs)
+
+addWorksheet(book, "UN_Regional_Groupings")
+writeData(book, sheet = "UN_Regional_Groupings",
+          x = regional, headerStyle = hs)
+
+addWorksheet(book, "World_Bank_Groupings")
+writeData(book, sheet = "World_Bank_Groupings", 
+          x = wb, headerStyle = hs)
+
+saveWorkbook(book, "Codes_Masterlist.xlsx", overwrite = TRUE)
